6a09cf1d07125a9f95c68eae555ca8f0aa7f5096
Stoyan Tsiparov <133793181+stoyantsiparov@users.noreply.github.com>
2026-04-07 15:28:40 +0300

    Add SpaProcedure and SpaRegistration entity models
    Introduce SpaProcedure entity with validation, scheduling, and pricing fields, and SpaRegistration as a join entity linking members to procedures. These models enable spa procedure management and member registration features.

---
+++ UNWE-FitnessCenterApp/FitnessCenterApp.Data.Models/SpaProcedure.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using static FitnessCenterApp.Common.EntityValidationConstants.SpaProcedure;
+
+namespace FitnessCenterApp.Data.Models;
+
+public class SpaProcedure : BaseEntity
+{
+    [Key]
+    [Comment("Primary key")]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(NameMaxLength)]
+    [Comment("Spa type")]
+    public string Name { get; set; } = null!;
+
+    [Comment("Image URL of the spa procedure")]
+    public string? ImageUrl { get; set; }
+
+    [Required]
+    [MaxLength(DescriptionMaxLength)]
+    [Comment("Description of the spa procedure")]
+    public string Description { get; set; } = null!;
+
+    [Required]
+    [Range(DurationMinValue, DurationMaxValue)]
+    [Comment("Duration of the spa procedure in minutes")]
+    public int Duration { get; set; }
+
+    [Required]
+    [Range(typeof(decimal), PriceMinValue, PriceMaxValue)]
+    [Comment("Price of the spa procedure")]
+    public decimal Price { get; set; }
+
+    [Required]
+    [DataType(DataType.DateTime)]
+    [DisplayFormat(DataFormatString = AppointmentDateTimeFormat, ApplyFormatInEditMode = true)]
+    [Comment("Appointment date and time for the spa service")]
+    public DateTime AppointmentDateTime { get; set; }
+
+    [Required]
+    [Range(CapacityMinValue, CapacityMaxValue)]
+    [Comment("Maximum capacity for the procedure")]
+    public int Capacity { get; set; } = CapacityDefaultValue;
+
+    [Comment("Registrations for this spa procedure")]
+    public virtual ICollection<SpaRegistration> SpaRegistrations { get; set; } = new List<SpaRegistration>();
+}
---
+++ UNWE-FitnessCenterApp/FitnessCenterApp.Data.Models/SpaRegistration.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitnessCenterApp.Data.Models;
+
+[PrimaryKey(nameof(MemberId), nameof(SpaProcedureId))]
+public class SpaRegistration
+{
+    [ForeignKey(nameof(Member))]
+    public string MemberId { get; set; } = null!;
+    public IdentityUser Member { get; set; } = null!;
+
+    [ForeignKey(nameof(SpaProcedure))]
+    public int SpaProcedureId { get; set; }
+    public SpaProcedure SpaProcedure { get; set; } = null!;
+}
