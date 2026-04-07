42c61068b7e4554cb9c7c6ce8243ba0cc849ca15
Stoyan Tsiparov <133793181+stoyantsiparov@users.noreply.github.com>
2026-04-07 15:27:22 +0300

    Add BaseEntity with ModifiedOn property for tracking changes
    Introduced an abstract BaseEntity class in FitnessCenterApp.Data.Models. This class includes a required ModifiedOn_22180022 DateTime property, defaulting to UTC now, with a database comment for tracking the last modification timestamp.

---
+++ UNWE-FitnessCenterApp/FitnessCenterApp.Data.Models/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitnessCenterApp.Data.Models;
+
+public abstract class BaseEntity
+{
+    [Required]
+    [Comment("Date and time of the last modification")]
+    public DateTime ModifiedOn_22180022 { get; set; } = DateTime.UtcNow;
+}
