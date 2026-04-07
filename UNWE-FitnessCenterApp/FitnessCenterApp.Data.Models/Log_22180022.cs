6bf3b923466f5fef1228e698533ba14ce0be1567
Stoyan Tsiparov <133793181+stoyantsiparov@users.noreply.github.com>
2026-04-07 15:27:42 +0300

    Add Log_22180022 entity for database change tracking
    Introduced the Log_22180022 entity class to represent a log table for tracking database changes. The class includes fields for Id, TableName, OperationType, and OperationDateTime, with validation and schema mapping attributes. Entity Framework Core comments and table mapping are used, and validation constants are referenced. Added TODOs for further field documentation.

---
+++ UNWE-FitnessCenterApp/FitnessCenterApp.Data.Models/Log_22180022.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using static FitnessCenterApp.Common.EntityValidationConstants.Log_22180022;
+
+namespace FitnessCenterApp.Data.Models;
+
+[Comment("Log table for tracking database changes")]
+[Table("log_22180022")]
+public class Log_22180022
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required] 
+    [MaxLength(TableNameMaxLength)]
+    //TODO: Add comment for TableName
+    public string TableName { get; set; } = null!;
+
+    [Required]
+    [MaxLength(OperationTypeMaxLength)]
+    //TODO: Add comment for OperationType
+    public string OperationType { get; set; } = null!;
+
+    [Required]
+    //TODO: Add comment for OperationDateTime
+    public DateTime OperationDateTime { get; set; } = DateTime.UtcNow;
+}
