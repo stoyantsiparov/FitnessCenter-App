68b14fc9ba36f9aba4a6800ee26cd7596ef3331b
Stoyan Tsiparov <133793181+stoyantsiparov@users.noreply.github.com>
2026-04-07 15:29:09 +0300

    Add FitnessEvent and FitnessEventRegistration entities
    Introduced FitnessEvent entity with validation, formatting, and schema comments to represent fitness events. Added FitnessEventRegistration as a join entity for member registrations, using a composite key and foreign key relationships. These models establish the foundation for event management and registration features.

---
+++ UNWE-FitnessCenterApp/FitnessCenterApp.Data.Models/FitnessEvent.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using static FitnessCenterApp.Common.EntityValidationConstants.FitnessEvent;
+
+namespace FitnessCenterApp.Data.Models;
+
+public class FitnessEvent : BaseEntity
+{
+    [Key]
+    [Comment("Primary key")]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(TitleMaxLength)]
+    [Comment("Title of the fitness event")]
+    public string Title { get; set; } = null!;
+
+    [Comment("Image URL of the fitness event")]
+    public string? ImageUrl { get; set; }
+
+    [Required]
+    [MaxLength(DescriptionMaxLength)]
+    [Comment("Description of the fitness event")]
+    public string Description { get; set; } = null!;
+
+    [Required]
+    [MaxLength(LocationMaxLength)]
+    [Comment("Location of the fitness event")]
+    public string Location { get; set; } = null!;
+
+    [Required]
+    [DataType(DataType.DateTime)]
+    [DisplayFormat(DataFormatString = DateTimeFormat, ApplyFormatInEditMode = true)]
+    [Comment("Start date of the fitness event")]
+    public DateTime StartDate { get; set; }
+
+    [Required]
+    [DataType(DataType.DateTime)]
+    [DisplayFormat(DataFormatString = DateTimeFormat, ApplyFormatInEditMode = true)]
+    [Comment("End date of the fitness event")]
+    public DateTime EndDate { get; set; }
+
+    [Required]
+    [Range(CapacityMinValue, CapacityMaxValue)]
+    [Comment("Maximum capacity for the event")]
+    public int Capacity { get; set; } = CapacityDefaultValue;
+
+    [Comment("Members registered for the fitness event")]
+    public virtual ICollection<FitnessEventRegistration> FitnessEventRegistrations { get; set; } = new List<FitnessEventRegistration>();
+}
---
+++ UNWE-FitnessCenterApp/FitnessCenterApp.Data.Models/FitnessEventRegistration.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitnessCenterApp.Data.Models;
+
+[PrimaryKey(nameof(MemberId), nameof(EventId))]
+public class FitnessEventRegistration
+{
+    [ForeignKey(nameof(Member))]
+    public string MemberId { get; set; } = null!;
+    public IdentityUser Member { get; set; } = null!;
+
+    [ForeignKey(nameof(FitnessEvent))]
+    public int EventId { get; set; }
+    public FitnessEvent FitnessEvent { get; set; } = null!;
+}
