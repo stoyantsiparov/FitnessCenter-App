c88ce72396d8e2ff590ba52c436bdd4ad16d1306
Stoyan Tsiparov <133793181+stoyantsiparov@users.noreply.github.com>
2026-04-08 17:04:01 +0300

    Add ConcurrencyCheck to ModifiedOn_22180022 property
    Added the [ConcurrencyCheck] attribute to the ModifiedOn_22180022 property in BaseEntity to enable concurrency control during database updates and prevent conflicting modifications.

--- UNWE-FitnessCenterApp/FitnessCenterApp.Data.Models/BaseEntity.cs
+++ UNWE-FitnessCenterApp/FitnessCenterApp.Data.Models/BaseEntity.cs
@@ -6,6 +6,7 @@ namespace FitnessCenterApp.Data.Models;
 public abstract class BaseEntity
 {
     [Required]
+    [ConcurrencyCheck]
     [Comment("Date and time of the last modification")]
     public DateTime ModifiedOn_22180022 { get; set; } = DateTime.UtcNow;
 }
