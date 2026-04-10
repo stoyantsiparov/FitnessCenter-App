dbf999f606a0235e272fee23dd5dada5f79e158c
Stoyan Tsiparov <133793181+stoyantsiparov@users.noreply.github.com>
2026-04-10 15:35:22 +0300

    Added BaseController

---
+++ UNWE-FitnessCenterApp/FitnessCenterApp.Web/Controllers/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitnessCenterApp.Web.Controllers;
+
+[Authorize]
+public class BaseController : Controller
+{
+    protected string GetUserId()
+    {
+        string userId = string.Empty;
+
+        if (User != null)
+        {
+            userId = User.FindFirstValue(ClaimTypes.NameIdentifier) ?? string.Empty;
+        }
+        return userId;
+    }
+}
