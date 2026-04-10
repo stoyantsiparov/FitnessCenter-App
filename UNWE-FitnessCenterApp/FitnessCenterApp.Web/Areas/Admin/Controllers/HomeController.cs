7eb490a8e1ff410f1de73bd14907a859cf89d2e1
Stoyan Tsiparov <133793181+stoyantsiparov@users.noreply.github.com>
2026-04-10 15:30:05 +0300

    Added HomeController for Admin

---
+++ UNWE-FitnessCenterApp/FitnessCenterApp.Web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using static FitnessCenterApp.Common.ApplicationsConstants;
+
+namespace FitnessCenterApp.Web.Areas.Admin.Controllers;
+
+[Area(AdminRole)]
+[Authorize(Roles = AdminRole)]
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
