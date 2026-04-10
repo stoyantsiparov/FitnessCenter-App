656500c732b73ad478cb2b3162015af16d9bc167
Stoyan Tsiparov <133793181+stoyantsiparov@users.noreply.github.com>
2026-04-10 15:36:50 +0300

    Added FitnessClassController

---
+++ UNWE-FitnessCenterApp/FitnessCenterApp.Web/Controllers/FitnessClassController.cs
@@ -0,0 +1,100 @@
+﻿using FitnessCenterApp.Services.Data.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using static FitnessCenterApp.Common.ErrorMessages.FitnessClass;
+using static FitnessCenterApp.Common.SuccessfulValidationMessages.FitnessClass;
+
+namespace FitnessCenterApp.Web.Controllers;
+
+public class FitnessClassController : BaseController
+{
+    private readonly IFitnessClassService _fitnessClassService;
+
+    public FitnessClassController(IFitnessClassService fitnessClassService)
+    {
+        _fitnessClassService = fitnessClassService;
+    }
+
+    [AllowAnonymous]
+    public async Task<IActionResult> Index(string? searchQuery = null, int pageNumber = 1, int pageSize = 6)
+    {
+        // Ensure page number and page size are valid
+        var model = await _fitnessClassService.GetAllClassesPaginationAsync(searchQuery, pageNumber, pageSize);
+
+        return View(model);
+    }
+
+    public async Task<IActionResult> MyClasses()
+    {
+        var userId = GetUserId();
+        var model = await _fitnessClassService.GetMyClassesAsync(userId);
+
+        return View(model);
+    }
+
+    [AllowAnonymous]
+    public async Task<IActionResult> Details(int id)
+    {
+        var model = await _fitnessClassService.GetClassDetailsAsync(id);
+
+        if (model == null)
+        {
+            TempData["ErrorMessage"] = FitnessClassDoesNotExist;
+            return RedirectToAction(nameof(Index));
+        }
+
+        return View(model);
+    }
+
+    public async Task<IActionResult> AddToMyClasses(int id)
+    {
+        var model = await _fitnessClassService.GetClassByIdAsync(id);
+
+        if (model == null)
+        {
+            TempData["ErrorMessage"] = FitnessClassDoesNotExist;
+            return RedirectToAction(nameof(Details), new { id });
+        }
+
+        var userId = GetUserId();
+
+        try
+        {
+            await _fitnessClassService.AddToMyClassesAsync(userId, model);
+            TempData["SuccessMessage"] = ClassAddedSuccessfully;
+        }
+        catch (InvalidOperationException ex)
+        {
+            TempData["ErrorMessage"] = ex.Message;
+            return RedirectToAction(nameof(Details), new { id });
+        }
+
+        return RedirectToAction(nameof(MyClasses));
+    }
+
+    public async Task<IActionResult> RemoveFromMyClasses(int id)
+    {
+        var model = await _fitnessClassService.GetClassByIdAsync(id);
+
+        if (model == null)
+        {
+            TempData["ErrorMessage"] = UserNotRegisteredForClass;
+            return RedirectToAction(nameof(MyClasses));
+        }
+
+        var userId = GetUserId();
+
+        try
+        {
+            await _fitnessClassService.RemoveFromMyClassesAsync(userId, model);
+            TempData["SuccessMessage"] = ClassAppointmentRemovedSuccessfully;
+        }
+        catch (InvalidOperationException ex)
+        {
+            TempData["ErrorMessage"] = ex.Message;
+            return RedirectToAction(nameof(MyClasses));
+        }
+
+        return RedirectToAction(nameof(MyClasses));
+    }
+}
