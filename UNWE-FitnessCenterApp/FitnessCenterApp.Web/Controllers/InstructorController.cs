003d487a78bf44fc1dba29c90ff7fab23c6f5550
Stoyan Tsiparov <133793181+stoyantsiparov@users.noreply.github.com>
2026-04-10 15:37:56 +0300

    Added InstructorController

---
+++ UNWE-FitnessCenterApp/FitnessCenterApp.Web/Controllers/InstructorController.cs
@@ -0,0 +1,41 @@
+﻿using FitnessCenterApp.Services.Data.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitnessCenterApp.Web.Controllers;
+
+public class InstructorController : BaseController
+{
+    private readonly IInstructorService _instructorService;
+
+    public InstructorController(IInstructorService instructorService)
+    {
+        _instructorService = instructorService;
+    }
+
+    [AllowAnonymous]
+    public async Task<IActionResult> Index(string? searchQuery = null)
+    {
+        var model = await _instructorService.GetAllInstructorsAsync(searchQuery);
+
+        return View(model);
+    }
+
+    [AllowAnonymous]
+    public async Task<IActionResult> Details(int id)
+    {
+        if (id <= 0)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+
+        var model = await _instructorService.GetInstructorDetailsAsync(id);
+
+        if (model == null)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+
+        return View(model);
+    }
+}
