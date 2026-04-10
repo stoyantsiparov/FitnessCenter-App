766f1911439a45539d3a0635b836c9a4d9a4fff7
Stoyan Tsiparov <133793181+stoyantsiparov@users.noreply.github.com>
2026-04-10 15:29:47 +0300

    Added FitnessEventsManagerController

---
+++ UNWE-FitnessCenterApp/FitnessCenterApp.Web/Areas/Admin/Controllers/FitnessEventsManagementController.cs
@@ -0,0 +1,113 @@
+﻿using FitnessCenterApp.Services.Data.Contracts;
+using FitnessCenterApp.Web.Controllers;
+using FitnessCenterApp.Web.ViewModels.FitnessEvent;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using static FitnessCenterApp.Common.ApplicationsConstants;
+using static FitnessCenterApp.Common.ErrorMessages.FitnessEvent;
+using static FitnessCenterApp.Common.SuccessfulValidationMessages.FitnessEvent;
+
+namespace FitnessCenterApp.Web.Areas.Admin.Controllers;
+
+[Area(AdminRole)]
+[Authorize(Roles = AdminRole)]
+public class FitnessEventsManagementController : BaseController
+{
+    private readonly IFitnessEventService _fitnessEventService;
+
+    public FitnessEventsManagementController(IFitnessEventService fitnessEventService)
+    {
+        _fitnessEventService = fitnessEventService;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var fitnessEvents = await _fitnessEventService.GetAllFitnessEventsAsync(null, 1, 100);
+        return View(fitnessEvents.FitnessEvents);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Add()
+    {
+        var model = await _fitnessEventService.GetFitnessEventForAddAsync();
+        return View(model);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Add(AddFitnessEventViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            TempData["ErrorMessage"] = InvalidData;
+            return View(model);
+        }
+
+        var userId = GetUserId();
+        await _fitnessEventService.AddFitnessEventAsync(model, userId);
+
+        TempData["SuccessMessage"] = FitnessEventAddedSuccessfully;
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Edit(int id)
+    {
+        var model = await _fitnessEventService.GetFitnessEventByIdAsync(id);
+
+        if (model != null)
+        {
+            return View(model);
+        }
+
+        TempData["ErrorMessage"] = FitnessEventNotFound;
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(EditFitnessEventViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            TempData["ErrorMessage"] = InvalidData;
+            return View(model);
+        }
+
+        try
+        {
+            var userId = GetUserId();
+            await _fitnessEventService.EditFitnessEventAsync(model, userId);
+
+            TempData["SuccessMessage"] = FitnessEventUpdatedSuccessfully;
+            return RedirectToAction(nameof(Index));
+        }
+        catch (InvalidOperationException ex)
+        {
+            TempData["ErrorMessage"] = ex.Message;
+            return View(model);
+        }
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var model = await _fitnessEventService.GetFitnessEventForDeleteAsync(id);
+
+        if (model != null)
+        {
+            return View(model);
+        }
+
+        TempData["ErrorMessage"] = FitnessEventNotFound;
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Delete(DeleteFitnessEventViewModel model)
+    {
+        var userId = GetUserId();
+        await _fitnessEventService.DeleteFitnessEventAsync(model.Id, userId);
+
+        TempData["SuccessMessage"] = FitnessEventDeletedSuccessfully;
+        return RedirectToAction(nameof(Index));
+    }
+}
