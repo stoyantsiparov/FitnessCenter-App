9eeccc815c9b87c9e672cea7ab635ef3685f6a2c
Stoyan Tsiparov <133793181+stoyantsiparov@users.noreply.github.com>
2026-04-10 15:38:09 +0300

    Added MembershipTypeController

---
+++ UNWE-FitnessCenterApp/FitnessCenterApp.Web/Controllers/MembershipTypeController.cs
@@ -0,0 +1,99 @@
+﻿using FitnessCenterApp.Services.Data.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using static FitnessCenterApp.Common.ErrorMessages.MembershipType;
+using static FitnessCenterApp.Common.SuccessfulValidationMessages.MembershipType;
+
+namespace FitnessCenterApp.Web.Controllers;
+
+public class MembershipTypeController : BaseController
+{
+    private readonly IMembershipTypeService _membershipTypeService;
+
+    public MembershipTypeController(IMembershipTypeService membershipTypeService)
+    {
+        _membershipTypeService = membershipTypeService;
+    }
+
+    [AllowAnonymous]
+    public async Task<IActionResult> Index()
+    {
+        var model = await _membershipTypeService.GetAllMembershipTypesAsync();
+
+        return View(model);
+    }
+
+    public async Task<IActionResult> MyMembershipType()
+    {
+        var userId = GetUserId();
+        var model = await _membershipTypeService.GetMyMembershipTypesAsync(userId);
+
+        return View(model);
+    }
+
+    [AllowAnonymous]
+    public async Task<IActionResult> Details(int id)
+    {
+        var model = await _membershipTypeService.GetMembershipTypeDetailsAsync(id);
+
+        if (model == null)
+        {
+            TempData["ErrorMessage"] = MembershipTypeDoesNotExist;
+            return RedirectToAction(nameof(Index));
+        }
+
+        return View(model);
+    }
+
+    public async Task<IActionResult> AddMyMembership(int id)
+    {
+        var model = await _membershipTypeService.GetMembershipTypeByIdAsync(id);
+
+        if (model == null)
+        {
+            TempData["ErrorMessage"] = MembershipTypeDoesNotExist;
+            return RedirectToAction(nameof(Details), new { id });
+        }
+
+        var userId = GetUserId();
+
+        try
+        {
+            await _membershipTypeService.AddMyMembershipAsync(userId, model);
+            TempData["SuccessMessage"] = MembershipPurchasedSuccessfully;
+        }
+        catch (InvalidOperationException ex)
+        {
+            TempData["ErrorMessage"] = ex.Message;
+            return RedirectToAction(nameof(Details), new { id });
+        }
+
+        return RedirectToAction(nameof(MyMembershipType));
+    }
+
+    public async Task<IActionResult> RemoveMyMembership(int id)
+    {
+        var model = await _membershipTypeService.GetMembershipTypeByIdAsync(id);
+
+        if (model == null)
+        {
+            TempData["ErrorMessage"] = MembershipTypeDoesNotExist;
+            return RedirectToAction(nameof(MyMembershipType));
+        }
+
+        var userId = GetUserId();
+
+        try
+        {
+            await _membershipTypeService.RemoveMyMembershipAsync(userId, model);
+            TempData["SuccessMessage"] = MembershipTypeRemovedSuccessfully;
+        }
+        catch (InvalidOperationException ex)
+        {
+            TempData["ErrorMessage"] = ex.Message;
+            return RedirectToAction(nameof(MyMembershipType));
+        }
+
+        return RedirectToAction(nameof(MyMembershipType));
+    }
+}
