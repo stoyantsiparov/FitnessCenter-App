94d8fa963321d53f8f3e611d1df1491fff21ab88
Stoyan Tsiparov <133793181+stoyantsiparov@users.noreply.github.com>
2026-04-10 15:30:17 +0300

    Added InstructorController for Admin

---
+++ UNWE-FitnessCenterApp/FitnessCenterApp.Web/Areas/Admin/Controllers/InstructorsManagementController.cs
@@ -0,0 +1,162 @@
+﻿using FitnessCenterApp.Services.Data.Contracts;
+using FitnessCenterApp.Web.Controllers;
+using FitnessCenterApp.Web.ViewModels.Instructor;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using static FitnessCenterApp.Common.ApplicationsConstants;
+using static FitnessCenterApp.Common.ErrorMessages.Instructor;
+using static FitnessCenterApp.Common.SuccessfulValidationMessages.Instructor;
+
+namespace FitnessCenterApp.Web.Areas.Admin.Controllers;
+
+[Area(AdminRole)]
+[Authorize(Roles = AdminRole)]
+public class InstructorsManagementController : BaseController
+{
+    private readonly IInstructorService _instructorService;
+
+    public InstructorsManagementController(IInstructorService instructorService)
+    {
+        _instructorService = instructorService;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var instructors = await _instructorService.GetAllInstructorsAsync();
+        return View(instructors);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Add()
+    {
+        var model = await _instructorService.GetInstructorForAddAsync();
+        return View(model);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Add(AddInstructorViewModel model)
+    {
+        if (model == null)
+        {
+            TempData["ErrorMessage"] = InstructorViewModelCannotBeNull;
+            return View(model);
+        }
+
+        if (!ModelState.IsValid)
+        {
+            TempData["ErrorMessage"] = InvalidData;
+            return View(model);
+        }
+
+        try
+        {
+            var userId = GetUserId();
+            if (string.IsNullOrEmpty(userId))
+            {
+                TempData["ErrorMessage"] = UserIdCannotBeEmpty;
+                return View(model);
+            }
+
+            await _instructorService.AddInstructorAsync(model, userId);
+            TempData["SuccessMessage"] = InstructorAddedSuccessfully;
+            return RedirectToAction(nameof(Index));
+        }
+        catch (Exception)
+        {
+            TempData["ErrorMessage"] = InstructorAddError;
+            return View(model);
+        }
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Edit(int id)
+    {
+        if (id <= 0)
+        {
+            TempData["ErrorMessage"] = InvalidInstructorId;
+            return RedirectToAction(nameof(Index));
+        }
+
+        var model = await _instructorService.GetInstructorByIdAsync(id);
+
+        if (model == null)
+        {
+            TempData["ErrorMessage"] = InstructorNotFound;
+            return RedirectToAction(nameof(Index));
+        }
+
+        return View(model);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(EditInstructorViewModel model)
+    {
+        if (model == null)
+        {
+            TempData["ErrorMessage"] = InstructorViewModelCannotBeNull;
+            return View(model);
+        }
+
+        if (!ModelState.IsValid)
+        {
+            TempData["ErrorMessage"] = InvalidData;
+            return View(model);
+        }
+
+        try
+        {
+            var userId = GetUserId();
+            await _instructorService.EditInstructorAsync(model, userId);
+            TempData["SuccessMessage"] = InstructorUpdatedSuccessfully;
+            return RedirectToAction(nameof(Index));
+        }
+        catch (Exception)
+        {
+            TempData["ErrorMessage"] = InstructorEditError;
+            return View(model);
+        }
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Delete(int id)
+    {
+        if (id <= 0)
+        {
+            TempData["ErrorMessage"] = InvalidInstructorId;
+            return RedirectToAction(nameof(Index));
+        }
+
+        var model = await _instructorService.GetInstructorForDeleteAsync(id);
+
+        if (model == null)
+        {
+            TempData["ErrorMessage"] = InstructorNotFound;
+            return RedirectToAction(nameof(Index));
+        }
+
+        return View(model);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Delete(DeleteInstructorViewModel model)
+    {
+        if (model == null || model.Id <= 0)
+        {
+            TempData["ErrorMessage"] = InvalidInstructorId;
+            return RedirectToAction(nameof(Index));
+        }
+
+        try
+        {
+            var userId = GetUserId();
+            await _instructorService.DeleteInstructorAsync(model.Id, userId);
+            TempData["SuccessMessage"] = InstructorDeletedSuccessfully;
+        }
+        catch (Exception)
+        {
+            TempData["ErrorMessage"] = InstructorDeleteError;
+        }
+
+        return RedirectToAction(nameof(Index));
+    }
+}
