d12b0ff853acf500c411f999cb426a2da9c1f8db
Stoyan Tsiparov <133793181+stoyantsiparov@users.noreply.github.com>
2026-04-10 13:49:39 +0300

    Added migrations

---
+++ UNWE-FitnessCenterApp/FitnessCenterApp.Data/Migrations/20260409192727_ChangedAdminRolePassword.cs
@@ -0,0 +1,467 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FitnessCenterApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedAdminRolePassword : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "FitnessClasses",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 19, 27, 26, 727, DateTimeKind.Utc).AddTicks(519));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "FitnessClasses",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 19, 27, 26, 727, DateTimeKind.Utc).AddTicks(529));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "FitnessClasses",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 19, 27, 26, 727, DateTimeKind.Utc).AddTicks(537));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "FitnessClasses",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 19, 27, 26, 727, DateTimeKind.Utc).AddTicks(543));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "FitnessClasses",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 19, 27, 26, 727, DateTimeKind.Utc).AddTicks(547));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "FitnessClasses",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 19, 27, 26, 727, DateTimeKind.Utc).AddTicks(553));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "FitnessEvents",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 19, 27, 26, 727, DateTimeKind.Utc).AddTicks(812));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "FitnessEvents",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 19, 27, 26, 727, DateTimeKind.Utc).AddTicks(821));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "FitnessEvents",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 19, 27, 26, 727, DateTimeKind.Utc).AddTicks(828));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "FitnessEvents",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 19, 27, 26, 727, DateTimeKind.Utc).AddTicks(835));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "FitnessEvents",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 19, 27, 26, 727, DateTimeKind.Utc).AddTicks(843));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "FitnessEvents",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 19, 27, 26, 727, DateTimeKind.Utc).AddTicks(851));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "Instructors",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 19, 27, 26, 727, DateTimeKind.Utc).AddTicks(895));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "Instructors",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 19, 27, 26, 727, DateTimeKind.Utc).AddTicks(954));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "Instructors",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 19, 27, 26, 727, DateTimeKind.Utc).AddTicks(956));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "Instructors",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 19, 27, 26, 727, DateTimeKind.Utc).AddTicks(958));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "Instructors",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 19, 27, 26, 727, DateTimeKind.Utc).AddTicks(960));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "Instructors",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 19, 27, 26, 727, DateTimeKind.Utc).AddTicks(962));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "MembershipTypes",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 19, 27, 26, 727, DateTimeKind.Utc).AddTicks(997));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "MembershipTypes",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 19, 27, 26, 727, DateTimeKind.Utc).AddTicks(999));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "MembershipTypes",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 19, 27, 26, 727, DateTimeKind.Utc).AddTicks(1001));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "MembershipTypes",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 19, 27, 26, 727, DateTimeKind.Utc).AddTicks(1003));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "SpaProcedures",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 19, 27, 26, 727, DateTimeKind.Utc).AddTicks(1039));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "SpaProcedures",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 19, 27, 26, 727, DateTimeKind.Utc).AddTicks(1044));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "SpaProcedures",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 19, 27, 26, 727, DateTimeKind.Utc).AddTicks(1050));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "SpaProcedures",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 19, 27, 26, 727, DateTimeKind.Utc).AddTicks(1055));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "SpaProcedures",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 19, 27, 26, 727, DateTimeKind.Utc).AddTicks(1060));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "SpaProcedures",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 19, 27, 26, 727, DateTimeKind.Utc).AddTicks(1066));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "FitnessClasses",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 16, 16, 2, 540, DateTimeKind.Utc).AddTicks(7980));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "FitnessClasses",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 16, 16, 2, 540, DateTimeKind.Utc).AddTicks(7990));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "FitnessClasses",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 16, 16, 2, 540, DateTimeKind.Utc).AddTicks(7996));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "FitnessClasses",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 16, 16, 2, 540, DateTimeKind.Utc).AddTicks(8001));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "FitnessClasses",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 16, 16, 2, 540, DateTimeKind.Utc).AddTicks(8008));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "FitnessClasses",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 16, 16, 2, 540, DateTimeKind.Utc).AddTicks(8013));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "FitnessEvents",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 16, 16, 2, 540, DateTimeKind.Utc).AddTicks(8179));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "FitnessEvents",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 16, 16, 2, 540, DateTimeKind.Utc).AddTicks(8186));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "FitnessEvents",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 16, 16, 2, 540, DateTimeKind.Utc).AddTicks(8231));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "FitnessEvents",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 16, 16, 2, 540, DateTimeKind.Utc).AddTicks(8239));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "FitnessEvents",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 16, 16, 2, 540, DateTimeKind.Utc).AddTicks(8246));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "FitnessEvents",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 16, 16, 2, 540, DateTimeKind.Utc).AddTicks(8253));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "Instructors",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 16, 16, 2, 540, DateTimeKind.Utc).AddTicks(8293));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "Instructors",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 16, 16, 2, 540, DateTimeKind.Utc).AddTicks(8295));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "Instructors",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 16, 16, 2, 540, DateTimeKind.Utc).AddTicks(8296));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "Instructors",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 16, 16, 2, 540, DateTimeKind.Utc).AddTicks(8298));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "Instructors",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 16, 16, 2, 540, DateTimeKind.Utc).AddTicks(8299));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "Instructors",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 16, 16, 2, 540, DateTimeKind.Utc).AddTicks(8301));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "MembershipTypes",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 16, 16, 2, 540, DateTimeKind.Utc).AddTicks(8337));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "MembershipTypes",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 16, 16, 2, 540, DateTimeKind.Utc).AddTicks(8339));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "MembershipTypes",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 16, 16, 2, 540, DateTimeKind.Utc).AddTicks(8341));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "MembershipTypes",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 16, 16, 2, 540, DateTimeKind.Utc).AddTicks(8343));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "SpaProcedures",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 16, 16, 2, 540, DateTimeKind.Utc).AddTicks(8378));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "SpaProcedures",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 16, 16, 2, 540, DateTimeKind.Utc).AddTicks(8384));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "SpaProcedures",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 16, 16, 2, 540, DateTimeKind.Utc).AddTicks(8392));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "SpaProcedures",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 16, 16, 2, 540, DateTimeKind.Utc).AddTicks(8398));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "SpaProcedures",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 16, 16, 2, 540, DateTimeKind.Utc).AddTicks(8403));
+
+            migrationBuilder.UpdateData(
+                schema: "22180022",
+                table: "SpaProcedures",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "ModifiedOn_22180022",
+                value: new DateTime(2026, 4, 9, 16, 16, 2, 540, DateTimeKind.Utc).AddTicks(8408));
+        }
+    }
+}
