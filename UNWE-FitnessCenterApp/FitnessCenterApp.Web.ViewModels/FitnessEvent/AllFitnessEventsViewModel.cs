e69b1fca5f0cabdb39058d57a1439d243c882700
Stoyan Tsiparov <133793181+stoyantsiparov@users.noreply.github.com>
2026-04-12 17:39:30 +0300

    Added Capacity prop

--- UNWE-FitnessCenterApp/FitnessCenterApp.Web.ViewModels/FitnessEvent/AllFitnessEventsViewModel.cs
+++ UNWE-FitnessCenterApp/FitnessCenterApp.Web.ViewModels/FitnessEvent/AllFitnessEventsViewModel.cs
@@ -8,4 +8,5 @@ public class AllFitnessEventsViewModel
     public string StartDateTime { get; set; } = null!;
     public string EndDateTime { get; set; } = null!;
     public string? ImageUrl { get; set; }
+    public int Capacity { get; set; }
 }
