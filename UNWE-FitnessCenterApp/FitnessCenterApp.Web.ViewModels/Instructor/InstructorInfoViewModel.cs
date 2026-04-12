2d9577742f6a1022c637d85cfd621bce14dcc5d9
Stoyan Tsiparov <133793181+stoyantsiparov@users.noreply.github.com>
2026-04-12 16:25:27 +0300

    Added InstructorInfoViewModel

---
+++ UNWE-FitnessCenterApp/FitnessCenterApp.Web.ViewModels/Instructor/InstructorInfoViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace FitnessCenterApp.Web.ViewModels.Instructor;
+
+public class InstructorInfoViewModel
+{
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Specialization { get; set; } = null!;
+    public string Bio { get; set; } = null!;
+    public string? ImageUrl { get; set; }
+}
